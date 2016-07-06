cbbc67b6f4056ce047d833b98903d621c4cd00f7
Tulio Navarro <tnavarrodesenvolvimento@gmail.com>
2016-07-06 17:38:15 -0300

    server.js

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.get('/', function(req, res){
 
 
 app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
